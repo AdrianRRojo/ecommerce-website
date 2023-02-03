5e9de75360dceb6330060f520f3ecd50e504051d
Adrian Rojo <rojoadrian16@gmail.com>
2023-02-03 11:05:41 -0600

    connected BrowserRouter / added Route / and /cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
+      </Router>
     </div>
   );
 }
