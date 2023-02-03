3e70e751ad03ec115909cb6d7692300f93b37183
Adrian Rojo <rojoadrian16@gmail.com>
2023-02-03 11:13:18 -0600

    added Shop link to Nav / added shopping cart logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {ShoppingCart} from 'phosphor-react'
 
 export default function Navbar(){
     return(
         <div>
-            NavBar
+            <div>
+                <Link to='/'>Shop</Link>
+                <Link to='/cart'>
+                    <ShoppingCart size={40}/>
+                </Link>
+            </div>
         </div>
     )
 }
