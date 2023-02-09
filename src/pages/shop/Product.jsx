801835068e4cfdde2da61e314aab9dd509714828
Adrian Rojo <rojoadrian16@gmail.com>
2023-02-08 22:04:25 -0600

    added styling to 'add to cart' button

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -6,9 +6,9 @@ export const Product = (props) => {
 
 
   return (
-    <div className="product">
+    <div className="">
       <img src={productImage} />
-      <div className="description">
+      <div className="">
         <p>
           <b>{productName}</b>
         </p>
@@ -16,7 +16,7 @@ export const Product = (props) => {
         
       </div>
       {/* Add tail wind hover style / and rounded buttons */}
-      <button>Add to cart</button>
+      <button className="bg-transparent hover:bg-violet-600 text-violet-700 font-semibold hover:text-white py-2 px-4 border border-violet-500 hover:border-transparent rounded">Add to cart</button>
     </div>
   );
 };
