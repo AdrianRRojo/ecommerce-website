52eb6bce756c7eb213ec217ea1908c6541a40481
Adrian Rojo <rojoadrian16@gmail.com>
2023-02-10 12:43:50 -0600

    changed button color

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -19,7 +19,7 @@ export const Product = (props) => {
         
       </div>
       {/* Add tail wind hover style / and rounded buttons */}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => addItemsToCart(id)}>Add to cart {amountOfItems > 0 && <> ({amountOfItems})</>}</button>
+      <button className="bg-slate-600 hover:bg-slate-900 text-white font-bold py-2 px-4 rounded-full" onClick={() => addItemsToCart(id)}>Add to cart {amountOfItems > 0 && <> ({amountOfItems})</>}</button>
     </div>
   );
 };
