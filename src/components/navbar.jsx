181d276230b7a02efb5e7a3b4b92e74f5e7cd720
Adrian Rojo <rojoadrian16@gmail.com>
2023-02-23 08:59:59 -0600

    centered icons in the about page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import {LinkedinLogo, GithubLogo, FileArrowDown, Code} from 'phosphor-react'
 import logo from '../components/Logo.png'
-import resume from '../components/AdrianRojo.pdf'
 import './navbar.css';
 
 export default function Navbar(){
@@ -30,35 +29,6 @@ export default function Navbar(){
                         <li className="p-3 xl:p-6">
                             <a href="/Login">
                                 <span>Account</span>
-                            </a>
-                        </li>
-                        <li className="p-3 xl:p-6">
-                            <a href="/cart">
-                                <span>Cart</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <nav className="hidden xl:contents">
-                    <ul class="flex items-center mr-4 lg:mr-6 xl:mr-8">
-                        <li class="p-1">
-                            <a href="https://github.com/AdrianRRojo" target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
-                                <GithubLogo size="24" />
-                            </a>
-                        </li>
-                        <li className="p-1">
-                            <a href="https://www.linkedin.com/in/adrianrrojo/" target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
-                                <LinkedinLogo size={24} />
-                            </a>
-                        </li>
-                        <li className="p-1">
-                            <a href={resume} target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
-                                <FileArrowDown size={24} />
-                            </a>
-                        </li>
-                        <li className="p-1">
-                            <a href="https://adrirojo.vercel.app/" target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
-                                <Code size={24} />
                             </a>
                         </li>
                     </ul>
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,7 @@
 import headshot from '../about/Headshots.png'
 import { Link } from 'react-router-dom'
+import {LinkedinLogo, GithubLogo, FileArrowDown, Code} from 'phosphor-react'
+import resume from '../about/AdrianRojo.pdf'
 export default function About(){
     return(
         <div className="bg-zinc-900">
@@ -15,6 +17,28 @@ export default function About(){
             <p>As a recent graduate of General Assembly's Software Engineering Immersive program, I specialize in web development and possess proficiency in various software development forms. I am currently seeking my first position and have designed a "mock" e-commerce site to showcase my skills in this area.</p>
             <br></br>
 
+            <ul class="flex justify-center items-center mr-4 lg:mr-6 xl:mr-8">
+                        <li class="p-1">
+                            <a href="https://github.com/AdrianRRojo" target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
+                                <GithubLogo size="24" />
+                            </a>
+                        </li>
+                        <li className="p-1">
+                            <a href="https://www.linkedin.com/in/adrianrrojo/" target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
+                                <LinkedinLogo size={24} />
+                            </a>
+                        </li>
+                        <li className="p-1">
+                            <a href={resume} target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
+                                <FileArrowDown size={24} />
+                            </a>
+                        </li>
+                        <li className="p-1">
+                            <a href="https://adrirojo.vercel.app/" target="_blank" className="inline-block rounded-full border p-2 hover:shadow-lg hover:border-opacity-0 duration-200 hover:-translate-y-0.5">
+                                <Code size={24} />
+                            </a>
+                        </li>
+                    </ul>
         <div className="mt-8 text-base text-zinc-600 dark:text-zinc-400">
             <div className="row">
                 <div className="col-sm-6 mb-3 mb-sm-0">
